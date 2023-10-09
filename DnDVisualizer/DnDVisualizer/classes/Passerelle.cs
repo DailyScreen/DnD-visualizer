7e6140b84f7435b009fdc47cf00e2d81d377b05f
rico517 <delasalle.sio.girard.e@gmail.com>
2023-10-09 17:29:54 +0200

    ajout code passerelle

--- DnDVisualizer/DnDVisualizer/classes/Passerelle.cs
+++ DnDVisualizer/DnDVisualizer/classes/Passerelle.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Xml;
 
 namespace DnDVisualizer
 {
     class Passerelle
     {
-    }
+        // méthode protégée statique pour obtenir un document XML à partir d'un flux de données en lecture
+        // paramètre unFluxEnLecture : un flux de données en lecture (System.IO.StreamReader)
+        // retourne : un document XML (System.Xml.Document)
+        protected static XmlReader getDocumentXML(StreamReader unFluxEnLecture)
+        {
+            try
+            {
+                XmlReader leDocument = XmlReader.Create(unFluxEnLecture);
+                return leDocument;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+    }    
 }
