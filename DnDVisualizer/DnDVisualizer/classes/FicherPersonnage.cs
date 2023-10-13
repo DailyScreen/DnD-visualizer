0fcda71bf0f590b17e9b5a69ac90435b657f3d85
rico517 <delasalle.sio.girard.e@gmail.com>
2023-10-13 10:55:14 +0200

    ajout des assesseur de FichierPersonnages

--- DnDVisualizer/DnDVisualizer/classes/FicherPersonnage.cs
+++ DnDVisualizer/DnDVisualizer/classes/FicherPersonnage.cs
@@ -21,7 +21,7 @@ namespace DnDVisualizer.classes
         private String _competence;
         private String _outilsProf;
         private String _langue;
-        private int[] _piece;
+        private int[] _piece; // pp,gp,ep,sp,bp
 
         private String _armure;
         private String _bouclier;
@@ -46,13 +46,39 @@ namespace DnDVisualizer.classes
 
         #region constructeur
         public FichierPersonnage(
-        string race, string classe, int niveau, string histoire,
-        int force, int dexterite, int constitution, int intelligence, int sagesse, int charme,
-        string competence, string outilsProf, string langue, int[] piece,
-        string armure, string bouclier, string arme, string arme2, string outils,
-        string items, string nom, string sexe, int age, int taille, int poids,
-        string alignement, int experience, string[] traits, string[] ideaux, string[] lien,
-        string[] defauts, string[] allies)
+            string race, 
+            string classe, 
+            int niveau, 
+            string histoire,
+            int force,
+            int dexterite, 
+            int constitution, 
+            int intelligence, 
+            int sagesse, 
+            int charme,
+            string competence, 
+            string outilsProf, 
+            string langue, 
+            int[] piece,
+            string armure, 
+            string bouclier, 
+            string arme, 
+            string arme2, 
+            string outils,
+            string items, 
+            string nom, 
+            string sexe, 
+            int age, 
+            int taille, 
+            int poids,
+            string alignement, 
+            int experience, 
+            string[] traits, 
+            string[] ideaux, 
+            string[] lien,
+            string[] defauts, 
+            string[] allies
+            )
         {
             _race = race;
             _classe = classe;
@@ -94,6 +120,205 @@ namespace DnDVisualizer.classes
         #endregion
 
         #region propriete
+        public string Race
+        {
+            get { return _race; }
+            set { _race = value; }
+        }
+
+        public string Classe
+        {
+            get { return _classe; }
+            set { _classe = value; }
+        }
+
+        public int Niveau
+        {
+            get { return _niveau; }
+            set { _niveau = value; }
+        }
+
+        public string Histoire
+        {
+            get { return _histoire; }
+            set { _histoire = value; }
+        }
+
+        public int Force
+        {
+            get { return _force; }
+            set { _force = value; }
+        }
+
+        public int Dexterite
+        {
+            get { return _dexterite; }
+            set { _dexterite = value; }
+        }
+
+        public int Constitution
+        {
+            get { return _constitution; }
+            set { _constitution = value; }
+        }
+
+        public int Intelligence
+        {
+            get { return _intelligence; }
+            set { _intelligence = value; }
+        }
+
+        public int Sagesse
+        {
+            get { return _sagesse; }
+            set { _sagesse = value; }
+        }
+
+        public int Charme
+        {
+            get { return _charme; }
+            set { _charme = value; }
+        }
+
+        public string Competence
+        {
+            get { return _competence; }
+            set { _competence = value; }
+        }
+
+        public string OutilsProf
+        {
+            get { return _outilsProf; }
+            set { _outilsProf = value; }
+        }
+
+        public string Langue
+        {
+            get { return _langue; }
+            set { _langue = value; }
+        }
+
+        public int[] Piece
+        {
+            get { return _piece; }
+            set { _piece = value; }
+        }
+
+        public string Armure
+        {
+            get { return _armure; }
+            set { _armure = value; }
+        }
+
+        public string Bouclier
+        {
+            get { return _bouclier; }
+            set { _bouclier = value; }
+        }
+
+        public string Arme
+        {
+            get { return _arme; }
+            set { _arme = value; }
+        }
+
+        public string Arme2
+        {
+            get { return _arme2; }
+            set { _arme2 = value; }
+        }
+
+        public string Outils
+        {
+            get { return _outils; }
+            set { _outils = value; }
+        }
+
+        public string Items
+        {
+            get { return _items; }
+            set { _items = value; }
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Sexe
+        {
+            get { return _sexe; }
+            set { _sexe = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public int Taille
+        {
+            get { return _taille; }
+            set { _taille = value; }
+        }
+
+        public int Poids
+        {
+            get { return _poids; }
+            set { _poids = value; }
+        }
+
+        public string Alignement
+        {
+            get { return _alignement; }
+            set { _alignement = value; }
+        }
+
+        public int Experience
+        {
+            get { return _experience; }
+            set { _experience = value; }
+        }
+
+        public string[] Traits
+        {
+            get { return _traits; }
+            set { _traits = value; }
+        }
+
+        public string[] Ideaux
+        {
+            get { return _ideaux; }
+            set { _ideaux = value; }
+        }
+
+        public string[] Lien
+        {
+            get { return _lien; }
+            set { _lien = value; }
+        }
+
+        public string[] Defauts
+        {
+            get { return _defauts; }
+            set { _defauts = value; }
+        }
+
+        public string[] Allies
+        {
+            get { return _allies; }
+            set { _allies = value; }
+        }
+        #endregion
+
+        #region methode
+
+        public static void sycroArgent()
+        {
+
+        }
         #endregion
     }
 }
--- DnDVisualizer/UnitTestProjectDND/UnitTestFichierPersonnage.cs
+++ DnDVisualizer/UnitTestProjectDND/UnitTestFichierPersonnage.cs
@@ -4,7 +4,7 @@ using System;
 namespace UnitTestProjectDND
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestFichierPersonnage
     {
         [TestMethod]
         public void TestMethod1()
